59d9f6c38feb61cbb4e1d377aefd7236f2dca642
Patty Baraibar <pattybaraibar@msn.com>
2021-07-24 20:25:40 -0500

    added form values in jQuery

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("on ready");
+$(document).ready(onReady);
+
+function onReady(){
+
+
+$('#submitEmployee').on('submit', onSubmit);
+}
+
+function onSubmit(event){
+    //prevents the page from re-loading
+    event.preventDefault();
+    console.log('onSubmit');
+
+//Getting values from form
+    let employee = {
+        firstName: $('#fname').val(),
+        lastName: $('#lname').val(),
+        ID: $('#idNumber').val(),
+        title: $('#employeeTitle').val(),
+        annualSalary: Number($('#annualSalary').val()),
+    };
+
+console.log('employee', employee);
+
+}
